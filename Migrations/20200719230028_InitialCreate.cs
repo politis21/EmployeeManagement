7d33d03a5372c7575675c1694c561f669ad0b6fb
Konstantinos Politis <kostispot@outlook.com>
2020-07-23 22:07:22 +0300

    My first commit!

---
+++ Migrations/20200719230028_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Asp.netCoreMVCCRUD.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FullName = table.Column<string>(nullable: false),
+                    EmpCode = table.Column<string>(nullable: true),
+                    Position = table.Column<string>(nullable: true),
+                    OfficeLocation = table.Column<string>(nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(9, 2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Asp.netCoreMVCCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column("FullName")]
+        [Required(ErrorMessage ="This field is required.")]
+        [DisplayName("Full Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""']*$"), StringLength(30, MinimumLength = 3)]
+        public string FullName { get; set; }
+        [Column("EmpCode")]
+        [DisplayName("Emp. Code")]
+        [RegularExpression(@"[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+        public string EmpCode { get; set; }
+        [Column("Position")]
+        [RegularExpression(@"^[A-Z]+[a-z]*$"), Required(ErrorMessage ="This field is required."), StringLength(30)]
+        public string Position { get; set; }
+        [Column("OfficeLocation")]
+        [DisplayName("Office Location")]
+        [RegularExpression(@"^[A-Z]+[a-z]*$"), StringLength(40)]
+        public string OfficeLocation { get; set; }
+        [Column("Salary", TypeName = "decimal(9, 2)")]
+        [DisplayFormat(DataFormatString ="{0:C}")]
+        public decimal Salary { get; set; }
+
+        //[Column("RecruitmentDate")]
+        //public DateTime RecruitmentDate  { get; set; }
+    }
+}
